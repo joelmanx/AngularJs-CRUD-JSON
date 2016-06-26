43eb3b6f8d8cd4cbec7f74e6ad47a5a40b3f64fd
jujomago@gmail.com <jujomago@gmail.com>
2016-06-25 22:18:39 -0400

    changed styles less, using angular filter

--- client/app/todolist/todolist.controller.js
+++ client/app/todolist/todolist.controller.js
@@ -5,14 +5,14 @@ class TodolistComponent {
   constructor() {
     this.message = 'Hello';
     this.alltodos=[
-    	{text:'learn es6',done:false},
-    	{text:'learn github',done:false},
-    	{text:'learn npm',done:false},
+    	{text:'LEARn es6',done:false},
+    	{text:'learn GIThub',done:false},
+    	{text:'LEARN npm',done:true},
     	{text:'learn node',done:false},
-    	{text:'learn angular',done:false},
-    	{text:'learn less',done:false},
-    	{text:'learn css',done:false},
-    	{text:'learn bootstrap',done:false}
+    	{text:'LEARN ANGULAR',done:false},
+    	{text:'leARN less',done:false},
+    	{text:'lEaRN css',done:false},
+    	{text:'learn BootStrap',done:false}
     ];
   }
 }
