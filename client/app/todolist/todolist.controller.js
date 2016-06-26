e34289d5a81bde912f527100ce0d58fd2418de37
jujomago@gmail.com <jujomago@gmail.com>
2016-06-25 20:58:04 -0400

    ng-repeat in todolist with model in controller

--- client/app/todolist/todolist.controller.js
+++ client/app/todolist/todolist.controller.js
@@ -4,13 +4,24 @@
 class TodolistComponent {
   constructor() {
     this.message = 'Hello';
+    this.alltodos=[
+    	{text:'learn es6',done:false},
+    	{text:'learn github',done:false},
+    	{text:'learn npm',done:false},
+    	{text:'learn node',done:false},
+    	{text:'learn angular',done:false},
+    	{text:'learn less',done:false},
+    	{text:'learn css',done:false},
+    	{text:'learn bootstrap',done:false}
+    ];
   }
 }
 
 angular.module('todoProjectApp')
   .component('todolist', {
     templateUrl: 'app/todolist/todolist.html',
-    controller: TodolistComponent
+    controller: TodolistComponent,
+    controllerAs: 'vm'
   });
 
 })();
